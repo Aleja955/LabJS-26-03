24301c815a09df41dd6cf7627de00474f1fe4209
Aleja955 <alejapaiva97@gmail.com>
2025-03-26 12:21:37 -0500

    Subiendo los documentos

---
+++ task5.js
@@ -0,0 +1,5 @@
+// Refer to Task 5 in your Instructions to complete this task
+const prompt = require("prompt-sync")();
+
+let PlabraUser = String(prompt("Escribe algo: "))
+console.log(PlabraUser);
